1b650d80b56897bcc271c3429ef1a1b4428477a8
User1 <anuradha.krishnanov7@gmail.com>
2019-08-30 19:04:37 +0530

    Release slots done By Anuradha

--- src/main/java/com/hcl/parking/controller/ParkingSlotBookingController.java
+++ src/main/java/com/hcl/parking/controller/ParkingSlotBookingController.java
@@ -3,6 +3,7 @@ package com.hcl.parking.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.hcl.parking.dto.ParkingSlotBookingInput;
 import com.hcl.parking.dto.ParkingSlotBookingOutput;
 import com.hcl.parking.service.ParkingSlotBooking;
-
+@CrossOrigin(allowedHeaders = { "*", "*/" }, origins = { "*", "*/" })
 @RestController
 public class ParkingSlotBookingController {
 
