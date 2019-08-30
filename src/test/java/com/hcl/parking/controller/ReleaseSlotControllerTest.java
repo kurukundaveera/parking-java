30a82b9aa89f49348e9c744053dbcf85370aaa49
User1 <anuradha.krishnanov7@gmail.com>
2019-08-30 19:42:34 +0530

    Release slots done By Anuradha

--- src/test/java/com/hcl/parking/controller/ReleaseSlotControllerTest.java
+++ src/test/java/com/hcl/parking/controller/ReleaseSlotControllerTest.java
@@ -1,14 +1,32 @@
 package com.hcl.parking.controller;
 
-import static org.junit.Assert.*;
-
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.hcl.parking.service.ReleaseSlotService;
+
+@RunWith(MockitoJUnitRunner.class)
 public class ReleaseSlotControllerTest {
+	@Mock
+	ReleaseSlotService releaseSlotService;
+	
+	@InjectMocks
+	ReleaseSlotController releaseSlotController;
+	MockMvc mockMvc;
+	
+	@Before
+	public void setup() {
+		mockMvc = MockMvcBuilders.standaloneSetup(releaseSlotController).build();
+	}
 
 	@Test
 	public void testReleaseSlot() {
-		fail("Not yet implemented");
 	}
 
 }
